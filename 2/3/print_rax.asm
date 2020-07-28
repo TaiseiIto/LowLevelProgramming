6748d3d8c75783c4568bb34d00344ce1d0d65efc
TaiseiIto <taisei1212@outlook.jp>
2020-07-28 19:45:17 +0900

    2.3

--- 2/3/print_rax.asm
+++ 2/3/print_rax.asm
@@ -2,6 +2,8 @@
 section .data
 codes:
 	db '0123456789ABCDEF'		; 16進数の表示に使う文字たち
+newline:
+	db 0x0a				; 最後に表示する改行文字
 
 section .text
 global _start
@@ -15,7 +17,7 @@ _start:
 	sub	rcx, 4			; ひとつ下の桁を表示するためにrcxから4引く
 	sar	rax, cl			; rax >>= cl;
 	and	rax, 0x000000000000000f	; rax &= 0x000000000000000f;
-	lea	rsi, [codes + rax]		; write system callの出力する文字列の先頭アドレス
+	lea	rsi, [codes + rax]	; write system callの出力する文字列の先頭アドレスrsi = codes + rax;
 	mov	rax, 1			; write system call
 	push	rcx			; rcxがwrite system callで書き換わるので待避
 	syscall				; write
@@ -23,6 +25,9 @@ _start:
 	pop	rax			; 表示内容復活
 	test	rcx, rcx		; rcx & rcxを計算してフラグを立てる
 	jnz	.loop			; rcx & rcxが0でない(rcx != 0)限り.loopに飛ぶ
+	mov	rax, 1			; 最後に改行文字を出力
+	lea	rsi, [newline]
+	syscall
 	mov	rax, 60			; exit system call
-	xor rdi, rdi			; このプログラムの返り値rdi ^= rdi (rdi = 0);
+	xor	rdi, rdi		; このプログラムの返り値rdi ^= rdi (rdi = 0);
 	syscall				; exit
